fffa71632eda30c7dab4199f6dd5712d0d14bbda
Anthony-EMILE-GUILLOT <a.emileguillot@gmail.com>
2025-11-05 13:03:45 +0100

    update v2.0.0

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,30 +4,23 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
-class LuckyController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    
+class LuckyController extends AbstractController
 {
     #[Route('/lucky/number')]
     public function number(): Response
     {
         $number = random_int(0, 100);
-        return new Response(
-                '<html>
-                <head>
-                <style>
-                body { 
-                    font-family: Comic Sans MS, sans-serif;
-                    font-size: 48px;
-                    background-color: #d1b6ffff;
-                    color: #000000ff;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-                </style>
-                </head>
-                <body>Lucky number: '.$number.'</body>
-                </html>'
-            );
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+
+    #[Route('/lucky/link')]
+    public function link(): Response
+    {
+        return $this->render('lucky/link.html.twig', []);
     }
 }
